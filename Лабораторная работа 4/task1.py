d6867608ec38cf939e3375fb7883490dcc4e80c0
kovriqt <115570503+kovriqt@users.noreply.github.com>
2023-02-12 21:43:37 +0300

    Update task1.py

--- Лабораторная работа 4/task1.py
+++ Лабораторная работа 4/task1.py
@@ -4,47 +4,47 @@ COLORS = ['dark', 'blue', 'yellow', 'purple', 'white']
 COLORS_1 = ['orange', 'green', 'indigo', 'pink', 'red ']
 
 
-class Guitars:
-    def __init__(self, string: int, manufacture: (int, str)):
+class T_shirt:
+    def __init__(self, size: int, manufacture: (int, str)):
         """
-        Создан родительский класс Guitars
-        :param string: количество струн
+        Создан родительский класс T-shirt
+        :param size: размер
         :param manufacture: производитель
         """
-        self.__string = 0
+        self.__size = 0
         self.__manufacture = 0
-        if self.__test(string) and self.__test(manufacture):
-            self.__string = string
+        if self.__test(size) and self.__test(manufacture):
+            self.__size = size
             self.__manufacture = manufacture
 
     @classmethod
-    def __test(cls, string):
-        return type(string) in (int, str)
+    def __test(cls, size):
+        return type(size) in (int, str)
 
     @property
-    def get_string(self):
+    def get_size(self):
         """
-        Создан интерфейсный метод (геттер) для возврата значения, установленного в атрибут self._sring
+        Создан интерфейсный метод (геттер) для возврата значения, установленного в атрибут self._size
         :return:
         """
-        return self.__string
+        return self.__size
 
-    @get_string.setter
-    def get_string(self, string):
-        if self.__test(string):
-            self.__string = string
+    @get_size.setter
+    def get_size(self, size):
+        if self.__test(size):
+            self.__size = size
         else:
             raise ValueError("Передаваемые значения должны быть типов \"int\" и \"str\"")
 
         """
-        Создан интерфейсный метод (сеттер) для установки значения атрибуту self._sring
-        :param new_string:
+        Создан интерфейсный метод (сеттер) для установки значения атрибуту self._size
+        :param new_size:
         :return:
         """
 
-    @get_string.deleter
-    def get_string(self):
-        del self.__string
+    @get_size.deleter
+    def get_size(self):
+        del self.__size
 
     @property
     def manufacture(self):
@@ -57,45 +57,45 @@ class Guitars:
     @manufacture.setter
     def manufacture(self, manufacture):
         if self.__test(manufacture):
-            self.__string = manufacture
+            self.__size = manufacture
         else:
             raise ValueError("Передаваемые значения должны быть типов \"int\" и \"str\"")
 
         """
         Создан интерфейсный метод (сеттер) для установки значения атрибуту self._manufacture
-        :param new_string:
+        :param new_size:
         :return:
         """
 
-    def paint_guitar(self):
+    def paint_T-shirt(self):
         """
-        Метод, который устанавливает цвет гитаре
+        Метод, который устанавливает цвет футболке
         :return: Случайный цвет
         """
         random_color = random.randint(0, len(COLORS) - 1)
         return COLORS[random_color]
 
     def __str__(self):
-        return f"Количество струн = {self.__string}, Производитель гитары - {self.manufacture}"
+        return f"размер = {self.__size}, Производитель футболки - {self.manufacture}"
 
     def __repr__(self):
-        return f"{self.__class__}: {self.__string!r}, {self.__manufacture!r}"
+        return f"{self.__class__}: {self.__size!r}, {self.__manufacture!r}"
 
 
-class Electric_guitars(Guitars):
+class Polo_shirt(T_shirt):
     """
-    Электрогитары - дочерний класс
+    Футболка поло - дочерний класс
     """
 
-    def __init__(self, string: int, manufacture: (int, str), model: str):
-        super().__init__(string, manufacture)
+    def __init__(self, size: int, manufacture: (int, str), model: str):
+        super().__init__(size, manufacture)
         self.model = model
 
     @property
     def model(self) -> str:
         """
         Создан геттер
-        :return: Должен вернуть модель электрогитары
+        :return: Должен вернуть модель поло
         """
         return self.model
 
@@ -103,21 +103,21 @@ class Electric_guitars(Guitars):
     def model(self, new_model) -> None:
         self._model = new_model
 
-    def paint_guitar(self):
+    def paint_T_shirt(self):
         """
-        Метод, который устанавливает цвет гитаре
+        Метод, который устанавливает цвет поло
         :return: Случайный цвет
         """
         random_color = random.randint(0, len(COLORS_1) - 1)
         return COLORS_1[random_color]
 
     def __str__(self):
-        return f"Количество струн = {self.__string}, Производитель гитары - {self.manufacture}, Модель корпуса - {self.model}"
+        return f"Размер = {self.__size}, Производитель поло - {self.manufacture}, Материал - {self.material}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}: {self.__string!r}, {self.manufacture!r}, {self.model}"
+        return f"{self.__class__.__name__}: {self.__size!r}, {self.manufacture!r}, {self.material}"
 
 
 if __name__ == "__main__":
-    telecaster = Electric_guitars(6, 'Fender', "Telecaster")
-    print(telecaster.paint_guitar())
+    telecaster = Polo_shirt(6, 'Fender', "Telecaster")
+    print(telecaster.paint_T_shirt())
